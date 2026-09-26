52f537450704e385cae9c7f372f55894c8acbc1d
arslan15 <asarslansaeed1@gmail.com>
2026-09-26 22:02:56 +0500

    issue  workaround fixed

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -267,7 +267,7 @@ export default function LoginForm({ users = [], setUsers, onLoginSuccess }) {
                 : !isLogin 
                   ? 'Sign Up' 
                   : loginStep === 1 
-                    ? 'Continue to OTP' 
+                    ? 'Login' 
                     : 'Verify & Sign In'}
             </button>
           </div>
